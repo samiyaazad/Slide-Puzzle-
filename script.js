32c96673e1892ad56cc9bb7c4da84d08885f1870
samiyaazad <samiya16azad@gmail.com>
2026-03-31 14:24:35 +0600

    puzzle

---
+++ script.js
@@ -0,0 +1,83 @@
+var rows = 3;
+var columns = 3;
+
+var currTile;
+var otherTile;
+
+var turns = 0;
+
+var imgOrder = ["4", "2", "8", "5", "1", "6", "7", "9", "3"];
+
+window.onload = function() {
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns; c++) {
+
+            let tile = this.document.createElement("img");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.src = imgOrder.shift() + ".jpg";
+
+            tile.addEventListener("dragstart", dragstart);
+            tile.addEventListener("dragover", dragOver);
+            tile.addEventListener("dragenter", dragEnter);
+            tile.addEventListener("dragleave", dragLeave);
+            tile.addEventListener("drop", dragDrop);
+            tile.addEventListener("dragend", dragEnd);
+
+            this.document.getElementById("board").append(tile);
+        }
+    }
+}
+
+function dragstart() {
+    currTile = this;
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+}
+
+function dragLeave() {
+
+}
+
+function dragDrop() {
+    otherTile = this;
+}
+
+function dragEnd() {
+    if (!otherTile.src.includes("3.jpg")) {
+        return;
+    }
+
+    let currCoords = currTile.id.split("-");
+    let r = parseInt(currCoords[0]);
+    let c = parseInt(currCoords[1]);
+
+    let otherCoords = otherTile.id.split("-");
+    let r2 = parseInt(otherCoords[0]);
+    let c2 = parseInt(otherCoords[1]);
+
+    let moveLeft = r == r2 && c2 == c-1;
+    let moveRight = r == r2 && c2 == c+1;
+
+    let moveUp = c2 == c && r2 == r-1;
+    let moveDown = c2 == c && r2 == r+1;
+
+    let isAdjacent = moveLeft || moveRight || moveUp || moveDown;
+
+     if (isAdjacent) {
+        let currImg = currTile.src;
+        let otherImg = otherTile.src;
+
+        currTile.src = otherImg;
+        otherTile.src = currImg;
+
+        turns += 1;
+        document.getElementById("turns").innerText = turns;
+    }
+
+}
